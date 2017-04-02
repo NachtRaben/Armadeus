e58f780975b16b2a156f5f45886166a155e24a6b
NachtRaben <stilleraben@gmail.com>
2017-04-02 10:03:34 -0400

    Respond to mentions now.

--- src/main/java/com/nachtraben/core/listeners/CommandListener.java
+++ src/main/java/com/nachtraben/core/listeners/CommandListener.java
@@ -4,6 +4,7 @@ import com.nachtraben.core.JDABot;
 import com.nachtraben.core.command.GuildCommandSender;
 import com.nachtraben.core.managers.GuildManager;
 import com.nachtraben.tohsaka.Tohsaka;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -11,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class CommandListener extends ListenerAdapter {
 
@@ -24,12 +26,17 @@ public class CommandListener extends ListenerAdapter {
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String content = event.getMessage().getRawContent();
+		List<User> mentions = event.getMessage().getMentionedUsers();
+
 		if (content != null && content.length() > 0) {
 			GuildCommandSender sender = new GuildCommandSender(event.getMessage());
 			GuildManager guildManager = GuildManager.getManagerFor(event.getGuild().getId());
 			String prefix = null;
-			if(guildManager.getConfig().getGuildPrefixes() != null && !guildManager.getConfig().getGuildPrefixes().isEmpty()) {
-				for(String s : guildManager.getConfig().getGuildPrefixes()) {
+			if (!mentions.isEmpty() && mentions.get(0).equals(event.getJDA().getSelfUser())) {
+				prefix = mentions.get(0).getAsMention() + " ";
+			}
+			if (guildManager.getConfig().getGuildPrefixes() != null && !guildManager.getConfig().getGuildPrefixes().isEmpty()) {
+				for (String s : guildManager.getConfig().getGuildPrefixes()) {
 					if (content.startsWith(s)) {
 						prefix = s;
 						break;
@@ -45,7 +52,7 @@ public class CommandListener extends ListenerAdapter {
 			}
 			//TODO Check guild prefixes first
 
-			if(Tohsaka.debug && !event.getAuthor().getId().equals("118255810613608451")) return;
+			if (Tohsaka.debug && !event.getAuthor().getId().equals("118255810613608451")) return;
 			if (prefix != null) {
 				String message = content.replaceFirst(prefix, "");
 				String[] tokens = message.split(" ");
@@ -57,6 +64,7 @@ public class CommandListener extends ListenerAdapter {
 					e.printStackTrace();
 				}
 			}
+			LOGGER.info(String.format("[%s#%s][%s#%s] %s", event.getGuild().getName(), event.getGuild().getId(), event.getMember().getEffectiveName(), event.getAuthor().getId(), content));
 		}
 	}
 
