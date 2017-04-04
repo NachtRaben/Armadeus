cfec2c03c186f165e1311320746ef92181745206
Comportment Last <Comportment@users.noreply.github.com>
2017-04-04 01:03:38 +0100

    👀

--- src/main/java/com/nachtraben/tohsaka/Tohsaka.java
+++ src/main/java/com/nachtraben/tohsaka/Tohsaka.java
@@ -18,32 +18,31 @@ import org.slf4j.LoggerFactory;
  */
 public class Tohsaka extends JDABot {
 
-	private static final Logger logger = LoggerFactory.getLogger(Tohsaka.class);
+    private static final Logger logger = LoggerFactory.getLogger(Tohsaka.class);
     public static boolean debug;
 
     public Tohsaka(boolean debug) {
         super.loadJDAs();
         Tohsaka.debug = debug;
         getCommandHandler().addEventListener((e) -> {
-        	if(e.getResult().equals(CommandEvent.Result.EXCEPTION)) {
-        		logger.info(e.toString());
-        		logger.info(e.getThrowable().getMessage(), e.getThrowable());
-			} else {
-        		if(e.getResult() != CommandEvent.Result.COMMAND_NOT_FOUND && e.getSender() instanceof GuildCommandSender) {
-        			GuildCommandSender sender = (GuildCommandSender) e.getSender();
-        			GuildManager manager = GuildManager.getManagerFor(sender.getGuild());
-        			if(manager.getConfig().shouldDeleteCommandMessages())
-        				sender.getMessage().delete().queue();
-
-				}
-				logger.debug(e.toString());
-			}
-		});
+            if(e.getResult().equals(CommandEvent.Result.EXCEPTION)) {
+                logger.info(e.toString());
+        	logger.info(e.getThrowable().getMessage(), e.getThrowable());
+		} else {
+        	if(e.getResult() != CommandEvent.Result.COMMAND_NOT_FOUND && e.getSender() instanceof GuildCommandSender) {
+        	    GuildCommandSender sender = (GuildCommandSender) e.getSender();
+        	    GuildManager manager = GuildManager.getManagerFor(sender.getGuild());
+        	    if(manager.getConfig().shouldDeleteCommandMessages())
+        	        sender.getMessage().delete().queue();
+		}
+		logger.debug(e.toString());
+		}
+	});
         super.getCommandHandler().registerCommands(new OwnerCommands());
         super.getCommandHandler().registerCommands(new AdminCommands());
         super.getCommandHandler().registerCommands(new AudioCommands());
         super.getCommandHandler().registerCommands(new MiscCommands());
-		super.getCommandHandler().registerCommands(new LogChannelCommands());
+	super.getCommandHandler().registerCommands(new LogChannelCommands());
         super.getCommandHandler().registerCommands(new CatGirlsCommand());
         super.getCommandHandler().registerCommands(new GuildOwnerCommands());
         super.getCommandHandler().registerCommands(new HelpCommand());
@@ -54,5 +53,4 @@ public class Tohsaka extends JDABot {
     public static void main(String[] args) {
         new Tohsaka(false);
     }
-
 }
