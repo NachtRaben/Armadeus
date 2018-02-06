94e54dbf6e2f10e29c9a9b75427b44603042c72e
NachtRaben <stilleraben@gmail.com>
2018-02-06 11:40:26 -0500

    Make rand public.

---
+++ TohsakaCore/src/main/java/com/nachtraben/tohsaka/commands/image/AbstractImageCommand.java
@@ -0,0 +1,30 @@
+package com.nachtraben.tohsaka.commands.image;
+
+import com.nachtraben.core.util.TimedCache;
+import com.nachtraben.orangeslice.command.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractImageCommand extends Command {
+
+    public static final Random RAND = new Random();
+    private static final Logger LOGGER = LoggerFactory.getLogger(KonachanCommand.class);
+
+    protected TimedCache<Long, Set<String>> guildSearchCache = new TimedCache<>(TimeUnit.MINUTES.toMillis(30), TimedCache.TimeoutPolicy.ACCESS);
+    protected TimedCache<Long, Set<String>> userSearchCache = new TimedCache<>(TimeUnit.MINUTES.toMillis(30), TimedCache.TimeoutPolicy.ACCESS);
+
+    /**
+     * Instantiates a new Command.
+     *
+     * @param name        the name
+     * @param format      the format
+     * @param description the description
+     */
+    public AbstractImageCommand(String name, String format, String description) {
+        super(name, format, description);
+    }
+}
--- TohsakaCore/src/main/java/com/nachtraben/tohsaka/commands/image/KonachanCommand.java
+++ TohsakaCore/src/main/java/com/nachtraben/tohsaka/commands/image/KonachanCommand.java
@@ -2,42 +2,27 @@ package com.nachtraben.tohsaka.commands.image;
 
 import com.nachtraben.core.command.DiscordCommandSender;
 import com.nachtraben.core.command.GuildCommandSender;
+import com.nachtraben.core.configuration.BotConfig;
 import com.nachtraben.core.util.ChannelTarget;
-import com.nachtraben.core.util.TimedCache;
 import com.nachtraben.core.util.Utils;
 import com.nachtraben.orangeslice.CommandSender;
-import com.nachtraben.orangeslice.command.Command;
 import com.vdurmont.emoji.EmojiParser;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.kodehawa.lib.imageboards.DefaultImageBoards;
 import net.kodehawa.lib.imageboards.entities.Rating;
 import net.kodehawa.lib.imageboards.entities.impl.KonachanImage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class KonachanCommand extends Command {
-
-    private static final Random RAND = new Random();
-    private static final Logger LOGGER = LoggerFactory.getLogger(KonachanCommand.class);
-    private TimedCache<Long, Set<String>> guildSearchCache = new TimedCache<>(TimeUnit.MINUTES.toMillis(30), TimedCache.TimeoutPolicy.ACCESS);
-    private TimedCache<Long, Set<String>> userSearchCache = new TimedCache<>(TimeUnit.MINUTES.toMillis(30), TimedCache.TimeoutPolicy.ACCESS);
+public class KonachanCommand extends AbstractImageCommand {
 
 
-    private final Random r = new Random();
 
     public KonachanCommand() {
         super("konachan", "(tag)", "Gets an image from Rule34.");
         super.setAliases(Arrays.asList("kona"));
-        super.setFlags(Arrays.asList("--safe", "--questionable", "--explicit", "-seq"));
+        super.setFlags(Arrays.asList("--safe", "--questionable", "--explicit", "-seq", "--amount="));
     }
 
     @Override
@@ -50,6 +35,10 @@ public class KonachanCommand extends Command {
             boolean questionable = flags.containsKey("questionable") || flags.containsKey("q");
             boolean explicit = flags.containsKey("explicit") || flags.containsKey("e");
             boolean isSearch = args.containsKey("tag");
+
+            BotConfig conf = sendee.getDbot().getConfig();
+            int amount = flags.containsKey("amount") && (conf.getOwnerIDs().contains(sendee.getUserID()) || conf.getDeveloperIDs().contains(sendee.getUserID())) ? Integer.parseInt(flags.get("amount")) : 1;
+
 
             Rating rating = gcs != null && gcs.getTextChannel().isNSFW() ? Rating.EXPLICIT : Rating.SAFE;
             if (explicit)
@@ -65,39 +54,43 @@ public class KonachanCommand extends Command {
             }
 
             Rating finalRating = rating;
-            List<KonachanImage> images = isSearch ? DefaultImageBoards.KONACHAN.search(100, args.get("tag").replace(" ", "_")).blocking() : DefaultImageBoards.KONACHAN.get(RAND.nextInt(1024)).blocking();
-            if (images != null) {
-                Set<String> cache = gcs != null ? guildSearchCache.computeIfAbsent(gcs.getGuildId(), set -> new HashSet<>()) : userSearchCache.computeIfAbsent(sendee.getUserID(), set -> new HashSet<>());
-                images = images.stream().filter(image -> finalRating.equals(image.getRating()) && image.getTags().stream().noneMatch(tag -> tag.equalsIgnoreCase("loli"))).filter(image -> !cache.contains(image.getURL())).collect(Collectors.toList());
-                if (images.isEmpty()) {
-                    if (isSearch) {
-                        if (!rating.equals(Rating.SAFE))
-                            sendee.sendMessage(ChannelTarget.NSFW, "Sorry, but I didn't find anything for the tag, `" + args.get("tag") + "`.");
-                        else
-                            sendee.sendMessage(ChannelTarget.GENERIC, "Sorry, but I didn't find anything for the tag, `" + args.get("tag") + "`.");
-                    } else {
-                        if (!rating.equals(Rating.SAFE))
-                            sendee.sendMessage(ChannelTarget.NSFW, "Sorry, but I wasn't able to find an image.");
-                        else
-                            sendee.sendMessage(ChannelTarget.GENERIC, "Sorry, but I wasn't able to find an image.");
+            // TODO: Abstractly handle the no image message.
+            // TODO: Abstract image getting
+            for (int i = 0; i < amount; i++) {
+                List<KonachanImage> images = isSearch ? DefaultImageBoards.KONACHAN.search(100, args.get("tag").replace(" ", "_")).blocking() : DefaultImageBoards.KONACHAN.get(RAND.nextInt(1024)).blocking();
+                if (images != null) {
+                    Set<String> cache = gcs != null ? guildSearchCache.computeIfAbsent(gcs.getGuildId(), set -> new HashSet<>()) : userSearchCache.computeIfAbsent(sendee.getUserID(), set -> new HashSet<>());
+                    images = images.stream().filter(image -> finalRating.equals(image.getRating()) && image.getTags().stream().noneMatch(tag -> tag.equalsIgnoreCase("loli"))).filter(image -> !cache.contains(image.getURL())).collect(Collectors.toList());
+                    if (images.isEmpty()) {
+                        if (isSearch) {
+                            if (!rating.equals(Rating.SAFE))
+                                sendee.sendMessage(ChannelTarget.NSFW, "Sorry, but I didn't find anything for the tag, `" + args.get("tag") + "`.");
+                            else
+                                sendee.sendMessage(ChannelTarget.GENERIC, "Sorry, but I didn't find anything for the tag, `" + args.get("tag") + "`.");
+                        } else {
+                            if (!rating.equals(Rating.SAFE))
+                                sendee.sendMessage(ChannelTarget.NSFW, "Sorry, but I wasn't able to find an image.");
+                            else
+                                sendee.sendMessage(ChannelTarget.GENERIC, "Sorry, but I wasn't able to find an image.");
+                        }
+                        return;
                     }
-                    return;
-                }
-                KonachanImage selection = images.get(RAND.nextInt(images.size()));
-                cache.add(selection.getURL());
-                EmbedBuilder eb = new EmbedBuilder();
-                eb.setColor(Utils.randomColor());
-                eb.setAuthor(isSearch ? args.get("tag") : "Konachan", selection.getURL(), null);
+                    KonachanImage selection = images.get(RAND.nextInt(images.size()));
+                    cache.add(selection.getURL());
+                    EmbedBuilder eb = new EmbedBuilder();
+                    eb.setColor(Utils.randomColor());
+                    eb.setAuthor(isSearch ? args.get("tag") : "Konachan", selection.getURL(), null);
 //                eb.setDescription("[link](" + selection.getImageUrl() + ")");
-                eb.setImage(selection.getURL());
-                eb.setFooter("Requested by: " + (gcs != null ? gcs.getMember().getEffectiveName() : sendee.getName()), sendee.getUser().getAvatarUrl());
-                if (finalRating.equals(Rating.SAFE))
-                    sendee.sendMessage(ChannelTarget.GENERIC, eb.build());
-                else
-                    sendee.sendMessage(ChannelTarget.NSFW, eb.build());
-            } else {
-                // TODO: Query error, could be invalid page, could be an actual outage. Re-Query?
-                sendee.sendMessage("Sorry, but I was unable to query the Yande.re, try again later.");
+                    eb.setImage(selection.getURL());
+                    eb.setFooter("Requested by: " + (gcs != null ? gcs.getMember().getEffectiveName() : sendee.getName()), sendee.getUser().getAvatarUrl());
+                    if (finalRating.equals(Rating.SAFE))
+                        sendee.sendMessage(ChannelTarget.GENERIC, eb.build());
+                    else
+                        sendee.sendMessage(ChannelTarget.NSFW, eb.build());
+                } else {
+                    // TODO: Query error, could be invalid page, could be an actual outage. Re-Query?
+                    sendee.sendMessage("Sorry, but I was unable to query the Yande.re, try again later.");
+                }
             }
         }
     }
