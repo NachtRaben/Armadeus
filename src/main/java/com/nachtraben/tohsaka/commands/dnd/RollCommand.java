c646b0e015a8e11707a97b35de918019d65ea770
NachtRaben <stilleraben@gmail.com>
2020-05-31 17:01:07 -0400

    Moar die

--- src/main/java/com/nachtraben/tohsaka/commands/dnd/RollCommand.java
+++ src/main/java/com/nachtraben/tohsaka/commands/dnd/RollCommand.java
@@ -27,15 +27,28 @@ public class RollCommand extends Command {
             int attempts = tokens.length == 2 ? Integer.parseInt(tokens[0]) : 1;
             int die = tokens.length == 2 ? Integer.parseInt(tokens[1]) : Integer.parseInt(tokens[0]);
             boolean percentage = flags.containsKey("p") || flags.containsKey("percentage");
+            boolean special = die == 4 || die == 8 || die == 10 || die == 12 || die == 20 || die == 100;
+            boolean regular = die < 6 && !special;
+            String image = null;
+            if (special) {
+                image = "https://rolladie.net/images/dice/d" + die + ".png";
+            } else if (regular) {
+                image = "https://rolladie.net/images/dice/dice" + die + ".png";
+            }
 
             EmbedBuilder builder = new EmbedBuilder();
             builder.setColor(Utils.randomColor());
-            builder.setThumbnail(args.get("die").contains("d") ? "https://rolladie.net/images/dice/d" + die + ".png" : null);
+            builder.setThumbnail(image);
             StringBuilder sb = new StringBuilder();
             sb.append("Rolls:\n");
+            int total = 0;
             for (int i = 0; i < attempts; i++) {
                 int roll = ThreadLocalRandom.current().nextInt(1, die + 1);
-                sb.append(i + 1).append(") ").append(percentage ? roll * 10 + "%" : roll).append("\n");
+                total += roll;
+                sb.append(i + 1).append(" ) ").append(percentage ? roll * 10 + "%" : roll).append("\n");
+            }
+            if (attempts > 1) {
+                sb.append("Total )").append(percentage ? total * 10 + "%" : total);
             }
             builder.setDescription(sb.toString());
             sendee.getMessageChannel().sendMessage(builder.build()).queue();
