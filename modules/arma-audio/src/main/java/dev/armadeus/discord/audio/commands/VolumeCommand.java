a04c0e34bb686ba81520fd4a421a4e21a95fd2c6
NachtRaben <stilleraben@gmail.com>
2021-09-13 17:26:43 -0400

    Formatting

--- modules/arma-audio/src/main/java/dev/armadeus/discord/audio/commands/VolumeCommand.java
+++ modules/arma-audio/src/main/java/dev/armadeus/discord/audio/commands/VolumeCommand.java
@@ -22,6 +22,6 @@ public class VolumeCommand extends AudioCommand {
         if (volume != null) {
             manager.getPlayer().setVolume(volume.floatValue() / 100.0f);
         }
-        user.sendMessage(String.format("The volume is currently `%.2f`%%", manager.getVolume()));
+        user.sendMessage(String.format("The volume is currently `%.2f`%%", manager.getVolume() * 100.0f));
     }
 }
